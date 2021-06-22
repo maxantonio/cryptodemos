f1b14a9d31568553db5a5e1c4c4029efb0338fe2
Tania <tbarrosolopez@gmail.com>
2021-06-22 12:25:39 -0400

    subiendo proyecto tanicoin

---
+++ tanicoin/contracts/TaniCoin.sol
@@ -0,0 +1,60 @@
+pragma solidity >=0.4.25 <0.7.0;
+
+contract TaniCoin {
+    string  public name = "Tani Coin";
+    string  public symbol = "TANI";
+    uint256 public totalSupply = 1000000000000000000000000; // 1 million tokens
+    uint8   public decimals = 18;
+    address payable public profit_address = 0xcd0a42BE7172275E63000654A2541E065aDd0c7c;
+
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
+
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    constructor() public {
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+
+        // 5% profit
+        uint256 profit = _value * 5 / 100;
+
+        // value to send
+        uint256 send = _value - profit;
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += send;
+        balanceOf[profit_address] += profit;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+}
