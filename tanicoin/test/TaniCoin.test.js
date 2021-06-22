17688da90878698e88593e203bc1c5bef044d89c
Tania <tbarrosolopez@gmail.com>
2021-06-22 18:02:23 -0400

    trabajando en las pruebas

--- tanicoin/test/TaniCoin.test.js
+++ tanicoin/test/TaniCoin.test.js
@@ -1,37 +1,39 @@
 const TaniCoin = artifacts.require('TaniCoin')
 
 require('chai')
-  .use(require('chai-as-promised'))
-  .should()
+    .use(require('chai-as-promised'))
+    .should()
 
 function tokens(n) {
-  return web3.utils.toWei(n, 'ether');
+    return web3.utils.toWei(n, 'ether');
 }
 
 contract('TaniCoin', (accounts) => {
-  let taniCoin
+    let taniCoin
 
-  before(async () => {
-    // Load Contracts
-    taniCoin = await TaniCoin.new()
-  })
-
-  describe('TaniCoin deployment', async () => {
-    it('has a name', async () => {
-      const name = await taniCoin.name()
-      assert.equal(name, 'Tani Coin')
+    before(async () => {
+        // Load Contracts
+        taniCoin = await TaniCoin.new()
     })
 
-    it('contract has tokens', async () => {
-      let balance = await taniCoin.balanceOf(accounts[0])
-      assert.equal(balance.toString(), tokens('1000000'))
-    })
+    describe('TaniCoin deployment', async () => {
+        it('has a name', async () => {
+            const name = await taniCoin.name()
+            assert.equal(name, 'Tani Coin')
+        })
 
-    it('make the transfer and subtract 5%', async () => {
-      await taniCoin.transfer(accounts[1], tokens('100'))
-      let send_balance = await taniCoin.balanceOf(accounts[1])
-      assert.equal(send_balance.toString(), tokens('95'))
+        it('contract has tokens', async () => {
+            let balance = await taniCoin.balanceOf(accounts[0])
+            assert.equal(balance.toString(), tokens('1000000'))
+        })
+
+        it('make the transfer and subtract 5%', async () => {
+            await taniCoin.transfer(accounts[1], tokens('100'))
+            let send_balance = await taniCoin.balanceOf(accounts[1])
+            let profit_balance = await taniCoin.balanceOf(accounts[9])
+            assert.equal(send_balance.toString(), tokens('95'))
+            assert.equal(profit_balance.toString(), tokens('5'))
+        })
     })
-  })
 
 })
