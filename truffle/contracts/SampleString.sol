0ff01d35649fd8e9a7b6624d67a8fc8f914ae790
Yoidel Martinez <yoidel86@gmail.com>
2021-10-04 16:31:46 -0400

    creando contrato ejemplo SampleString.sol

---
+++ truffle/contracts/SampleString.sol
@@ -0,0 +1,16 @@
+pragma solidity >=0.4.24 <0.7.0;
+
+contract SampleString{
+    string value;
+    constructor() public{
+        value = "Initial Value";
+    }
+
+    function get() public view returns(string){
+        return value;
+    }
+    function set(string _value) public {
+        value = _value;
+    }
+
+}
